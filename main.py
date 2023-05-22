60f5dd0cd5dae57c70df15558cf3b65626410a6c
Nastya <emelyanova.am2004@gmail.com>
2023-05-22 10:07:50 +0300

    прокрутка одной итерации по нажатии клавиш

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     width = int(input('Введите ширину поля: '))
     height = int(input('Введите длину поля: '))
     life_fraction = int(input('Введите долю живых клеток: '))
-    scale = int(input('Введите размер клеток при отрисовке: '))
+    scale = int(input('Введите размер клеток при рисовке: '))
     gof = GameOfLife(width, height)
     gof.initialize(life_fraction)
     pygame.init()
@@ -29,6 +29,8 @@ def main():
     iteration_number = 0
     is_running = True
     is_paused = True
+    is_iteration_mode = False
+    do_iteration = False
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -39,9 +41,9 @@ def main():
                         is_paused = False
                     else:
                         is_paused = True
-                elif event.type == pygame.K_ESCAPE:
+                elif event.key == pygame.K_ESCAPE:
                     is_running = False
-                elif event.type == pygame.K_BACKSPACE:
+                elif event.key == pygame.K_BACKSPACE:
                     gof = GameOfLife(width, height)
                     gof.initialize(life_fraction)
                     pygame.init()
@@ -49,10 +51,20 @@ def main():
                     screen = pygame.display.set_mode((width * scale + 400, height * scale + 110))
                     iteration_number = 0
                     is_paused = True
+                elif event.key == pygame.K_RETURN:
+                    if not is_iteration_mode:
+                        is_iteration_mode = True
+                    else:
+                        is_iteration_mode = False
+                elif event.key == pygame.K_DOWN:
+                    if is_iteration_mode:
+                        do_iteration = True
             if event.type == pygame.MOUSEBUTTONDOWN:
                 cursor_pos = event.pos
-                x_pos = cursor_pos[0] // 15
-                y_pos = cursor_pos[1] // 15
+                x_pos = cursor_pos[0] // scale
+                y_pos = cursor_pos[1] // scale
+                if y_pos > len(gof.field) - 1 or x_pos > len(gof.field[0]) - 1:
+                    continue
                 new_state = gof.field[y_pos][x_pos]
                 if event.button == 1:
                     new_state = 1
@@ -60,14 +72,20 @@ def main():
                     new_state = 0
                 gof.field[y_pos][x_pos] = new_state
         if not is_paused:
+            if is_iteration_mode:
+                if do_iteration:
+                    do_iteration = False
+                else:
+                    continue
+            iteration_number += 1
             gof.run_transition_rule()
-        screen.fill((0, 0, 0))
-        render_pygame(gof.field, screen)
-        text1 = main_font.render('I love python', True, (255, 255, 255))
-        screen.blit(text1, (10, 610))
-        pygame.display.flip()
-        clock.tick(60)
-        pygame.time.delay(200)
+            screen.fill((0, 0, 0))
+            render_pygame(gof.field, screen)
+            text1 = main_font.render('I love python', True, (255, 255, 255))
+            screen.blit(text1, (10, 610))
+            pygame.display.flip()
+            clock.tick(60)
+            pygame.time.delay(200)
 
 
 if __name__ == '__main__':
