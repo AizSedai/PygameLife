3ba739ceb3c49bf85235aed8f609aaa8059e5930
Nastya <emelyanova.am2004@gmail.com>
2023-05-22 09:05:25 +0300

    осуществили вывод текста на экран

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ def main():
     gof = GameOfLife(width, height)
     gof.initialize(life_fraction)
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Game Of Life')
     clock = pygame.time.Clock()
+    main_font = pygame.font.Font(None, 24)
     is_running = True
     is_paused = True
     while is_running:
@@ -49,6 +51,8 @@ def main():
             gof.run_transition_rule()
         screen.fill((0, 0, 0))
         render_pygame(gof.field, screen)
+        text1 = main_font.render('I love python', True, (255, 255, 255))
+        screen.blit(text1, (10, 610))
         pygame.display.flip()
         clock.tick(60)
         pygame.time.delay(200)
